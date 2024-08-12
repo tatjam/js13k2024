dab8f0f5554324e2cfd11ed3b31a028b5d9fdc1d
tatjam <tatjam02@gmail.com>
2024-08-12 22:24:40 +0200

    Babylon.js webxr template works

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,33 @@
 // script
+const canvas = document.getElementById("c"); // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+
+var createScene = function () {
+	var scene = new BABYLON.Scene(engine);
+	var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+	camera.setTarget(BABYLON.Vector3.Zero());
+	camera.attachControl(canvas, true);
+	var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+	light.intensity = 0.7;
+	var sphere = BABYLON.MeshBuilder.CreateSphere("sphere1", { segments: 16, diameter: 2 }, scene);
+	sphere.position.y = 1;
+
+	const env = scene.createDefaultEnvironment();
+
+	// here we add XR support
+	const xr = scene.createDefaultXRExperienceAsync({
+		floorMeshes: [env.ground],
+	});
+
+	return scene;
+};
+
+const scene = createScene(); //Call the createScene function
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function () {
+	scene.render();
+});
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+	engine.resize();
+});
